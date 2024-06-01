1b6daa9a04d652d8b57db7a20f419d6bc6c4d96c
Rostislav Molchanov <rmolchanov@hotmail.com>
2024-06-01 15:45:02 +0400

    изменения доп1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿////
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//int a = 4;
+//int b = 5;
+//if (a > 1)
+//{
+//    if (b > 2)
+//    {
+//        int SUM = a + b;
+//        Console.WriteLine(sum);
+//    }
+//}
+
+//public partial class MainClass
+//{
+//    public double Add(double a, double b
+//        {
+//        return a + b;
+//    })
+//}
+//public partial class MainClass
+//{
+//    public double Add(double a, double b
+//        {
+//        return a - b;
+//    })
+//}
+//public partial class MainClass
+//{
+//    public double Add(double a, double b
+//        {
+//        return a * b;
+//    })
+//}
+//public partial class MainClass
+//{
+//    public double Add(double a, double b
+//        {
+//        return a / b;
+//    })
+//}
+//public partial class MainClass
+//{
+//    public double Add(double a, double b
+//        {
+//        return a * b;
+//    })
+//}
+////
+
+string r = "Rost";
+string h = "hello";
+if (r == h)
+{
+    Console.WriteLine("одинковые значения");
+}
